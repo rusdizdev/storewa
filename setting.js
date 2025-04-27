4049591ba4006e48ad5e26f959de9e67bbc6d7f0
Rusdi Dev <rusdizdev@gmail.com>
2025-04-27 08:28:48 +0800

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,36 @@
+// setting.js
+module.exports = {
+    // Konfigurasi Owner
+    owner: {
+        number: "6287765027594@s.whatsapp.net",  // Format: 62xxxxxxxxxx@s.whatsapp.net
+        displayName: "Owner Bot"  // Nama tampilan untuk kontak owner
+    },
+
+    // Konfigurasi Menu
+    menu: {
+        imageUrl: "https://t1.pixhost.to/thumbs/5226/590983598_rusdidev.jpg",
+        text: `📌 *Menu Bot WhatsApp*\n\n` +
+              `📝 *List Commands:*\n` +
+              `- /addlist [key|value]\n` +
+              `- /tourl\n` +
+              `- /dellist [key]\n` +
+              `- /list\n\n` +
+              `👋 *Welcome Message:*\n` +
+              `- /welcome on/off\n` +
+              `- /chenwelcome [pesan]\n` +
+              `- /chenwelcome del\n\n` +
+              `🛡️ *Owner Cmd:*\n` +
+              `- /self on/off\n` +
+              `- /addadmin @user/628xx\n` +
+              `- /unadmin @user/628xx\n` +
+              `- /owner\n\n` +
+              `🛡️ *Admin Tools:*\n` +
+              `- /antilink on/off\n` +
+              `- /hidetag [pesan]\n` +
+              `- /kick [reply user]\n` +
+              `- /open\n` +
+              `- /close\n` +
+              `- /jeda [waktu]`,
+        footer: "©Zenz Store"  // Teks footer opsional
+    }
+};
